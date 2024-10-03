b8f3050db1d6ec0da93a20e0e3c5bc11f2c7b4ff
Neha220154 <nehat262004@gmail.com>
2024-10-03 07:02:10 +0530

    Create admin1.php

---
+++ admin1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Admin Dashboard</title>
+  <link rel="stylesheet" href="style-admin.css">
+</head>
+<body>
+  <div class="container">
+    <header>
+      <h1>Admin Dashboard</h1>
+    </header>
+    <div class="dashboard">
+      <div class="menu">
+        <ul>
+          <li><a href="users.php">Users</a></li>
+          <li><a href="sellers.php">Sellers</a></li>
+          <li><a href="products.php">Products</a></li>
+          <li><a href="orders.php">Orders</a></li>
+          <li><a href="reports.php">Reports</a></li>
+          <li><a href="settings.php">Settings</a></li>
+        </ul>
+      </div>
+      <div class="content">
+        Welcome to the Admin Dashboard!
+      </div>
+    </div>
+  </div>
+</body>
+</html>
